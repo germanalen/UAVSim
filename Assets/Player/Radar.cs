aa668e461e5e3ae71c868debde9844cdea2c1d28
alen <germanalen@gmail.com>
2017-07-31 13:36:34 +0600

    Added radar

---
+++ Assets/Player/Radar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+	public Mesh ringMesh;
+	public Material ringMaterial;
+	public float ringDistance = 2f;
+
+	void Update ()
+	{
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+
+		for (int i = 0; i < players.Length; ++i) {
+			if (players [i] != transform.parent.gameObject) {
+
+
+				Vector3 playerCenter = players [i].transform.Find ("Center").position;
+				Vector3 direction = (playerCenter - transform.position).normalized;
+				Vector3 ringPos = transform.position + direction * ringDistance;
+				Quaternion ringRot = Quaternion.LookRotation (direction);
+
+				Graphics.DrawMesh (ringMesh, ringPos, ringRot, ringMaterial, 0, null, 0, null, false, false, false); 
+			}
+		}
+	}
+}
